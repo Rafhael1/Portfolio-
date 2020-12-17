3654ea624a87cfa92efdac050cb77b898c06ea29
Rafhael <rafamlf.2011@gmail.com>
2020-12-17 16:10:30 -0300

    bah

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 
 app.use(express.static("src/public"));
-
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'))
@@ -71,7 +72,18 @@ app.get('/contact', function(req, res){
 
 ////////////////////////////////
 
-const port = process.env.PORT || 3000
+////////////////////////////////
+/////////////////////////////////
+      // Projects Page //
+/////////////////////////////////
+
+app.get('/projects', function(req, res){
+  res.render('./projects/projectsEN')
+})
+
+////////////////////////////////
+
+const port = process.env.PORT || 8080
 app.listen(port, function(){
     console.log(`Server has started at http://localhost:${port}`)
   })
